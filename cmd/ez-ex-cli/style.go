0177c471d732c5d0f176caecf742977e0267e311
armanimichael <25107976+armanimichael@users.noreply.github.com>
2023-12-08 19:55:00 +0100

    Create new styles for success messages and inputs

--- cmd/ez-ex-cli/style.go
+++ cmd/ez-ex-cli/style.go
@@ -9,6 +9,7 @@ const (
 	selectedForeground = lipgloss.Color("229")
 	selectedBackground = lipgloss.Color("32")
 	errorForeground    = lipgloss.Color("124")
+	successForeground  = lipgloss.Color("2")
 )
 
 var baseStyle = lipgloss.NewStyle().
@@ -18,8 +19,18 @@ var baseStyle = lipgloss.NewStyle().
 var errorMessageStyle = lipgloss.NewStyle().
 	Foreground(errorForeground)
 
+var successMessageStyle = lipgloss.NewStyle().
+	Foreground(successForeground)
+
 var keySuggestionStyle = lipgloss.NewStyle().
 	Foreground(lipgloss.Color("248"))
 
-var keySuggestionNoteStyle = lipgloss.NewStyle().
+var lowOpacityForegroundStyle = lipgloss.NewStyle().
 	Foreground(lipgloss.Color("240"))
+
+var inputBoxSelectedStyle = lipgloss.NewStyle().
+	Border(lipgloss.NormalBorder(), false, false, false, true).
+	BorderForeground(selectedBackground)
+
+var inputBoxStyle = lipgloss.NewStyle().
+	PaddingLeft(1)
